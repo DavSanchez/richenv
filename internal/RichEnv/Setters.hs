6fa7de84b2e522490e7e693ff4e7b1c1d15f83ad
David Sánchez <davidslt+git@pm.me>
2023-09-08 01:20:06 +0100

    feat: no-op on empty var names (#6)

--- internal/RichEnv/Setters.hs
+++ internal/RichEnv/Setters.hs
@@ -1,14 +1,16 @@
 module RichEnv.Setters (setPrefixedVars, setVarMapValues, setVarValueEnv) where
 
+import Control.Monad (unless)
 import Data.Bifunctor (first)
 import Data.HashSet (HashSet)
 import Data.HashSet qualified as S
 import Data.Maybe (mapMaybe)
 import Data.Text (stripPrefix, unpack)
+import Data.Text qualified as T
 import RichEnv.Types (Environment, VarMap (..), VarPrefix (..), VarValue (..))
 import System.Environment (setEnv)
 
--- | Takes 'VarValue's and sets them as environment variables.
+-- | Takes 'VarValue's and sets them as environment variables. It is a no-op if the variable name is empty.
 --
 -- >>> import System.Environment
 -- >>> value <- setVarValueEnv (VarValue "foo" "bar") >> getEnv "foo"
@@ -19,16 +21,23 @@ import System.Environment (setEnv)
 -- >>> value <- setVarValueEnv (VarValue "foo" "bar") >> getEnv "foo"
 -- >>> value == "bar"
 -- True
+-- >>> import System.Environment
+-- >>> env <- getEnvironment
+-- >>> setVarValueEnv (VarValue "" "bar")
+-- >>> newEnv <- getEnvironment
+-- >>> env == newEnv
+-- True
 setVarValueEnv :: VarValue -> IO ()
-setVarValueEnv vv =
+setVarValueEnv vv = do
   let name = vvName vv
       value = vvValue vv
-   in setEnv (unpack name) (unpack value)
+  unless (T.null name) $ setEnv (unpack name) (unpack value)
 
 -- | Takes an environment list and all the 'VarMap's and prepares a valid @HashSet@ of 'VarValue's according to the RichEnv rules.
 setVarMapValues :: Environment -> HashSet VarMap -> HashSet VarValue
 setVarMapValues cEnv = foldr setVarMapValue mempty
   where
+    setVarMapValue (VarMap "" _) = id
     setVarMapValue vm = do
       let name = vmName vm
           from = vmFrom vm
