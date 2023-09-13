b687ab02fea56f62c3d03675a5b2e21deefad252
David Sánchez <davidsanchez@newrelic.com>
2023-09-13 16:06:30 +0100

    ci: run cabal test on GHA

--- default.nix
+++ default.nix
@@ -1,18 +1,37 @@
-{ mkDerivation, aeson, base, bytestring, hashable, hspec
-, hspec-discover, lib, QuickCheck, unordered-containers, yaml
+{
+  mkDerivation,
+  aeson,
+  base,
+  bytestring,
+  hashable,
+  hspec,
+  hspec-discover,
+  lib,
+  QuickCheck,
+  unordered-containers,
+  yaml,
 }:
 mkDerivation {
   pname = "richenv";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base hashable unordered-containers
+    aeson
+    base
+    hashable
+    unordered-containers
   ];
   testHaskellDepends = [
-    aeson base bytestring hashable hspec QuickCheck
-    unordered-containers yaml
+    aeson
+    base
+    bytestring
+    hashable
+    hspec
+    QuickCheck
+    unordered-containers
+    yaml
   ];
-  testToolDepends = [ hspec-discover ];
+  testToolDepends = [hspec-discover];
   doHaddock = false;
   homepage = "https://github.com/DavSanchez/richenv";
   description = "Rich environment variable setup for Haskell";
