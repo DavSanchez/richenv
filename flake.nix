24f95510b27983edc179f56e4552e5ab844dd029
David Sánchez <davidslt+git@pm.me>
2023-09-16 01:30:37 +0100

    ci: set up caching for cabal

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
               ormolu.enable = true;
               hlint.enable = true;
               hpack.enable = true;
-              yamllint.enable = true;
+              # yamllint.enable = true;
               # hunspell.enable = true;
             };
             settings = {
