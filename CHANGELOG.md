c9c156b7d5051e6210bf0ef054d9859555d21069
David Sánchez <davidslt+git@pm.me>
2023-09-25 19:03:50 +0100

    docs: fix changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for richenv
 
-## 0.1.0.0 -- YYYY-mm-dd
+## 0.1.0.1 -- 2023-09-25
+
+* Fix changelog.
+
+## 0.1.0.0 -- 2023-09-25
 
 * First version. Released on an unsuspecting world.
--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "richenv";
-  version = "0.1.0.0";
+  version = "0.1.0.1";
   src = ./.;
   libraryHaskellDepends = [ aeson base text unordered-containers ];
   testHaskellDepends = [
