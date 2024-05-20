d536279f33e445a80792131084fe370f0984defa
David Sánchez <davidslt+git@pm.me>
2024-05-20 22:18:31 +0100

    chore: bump version

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "richenv";
-  version = "0.1.0.2";
+  version = "0.1.0.3";
   src = ./.;
   libraryHaskellDepends = [ aeson base text unordered-containers ];
   testHaskellDepends = [
