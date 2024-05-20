03fbfb992e68d1ed40343316b0c807484360c985
David Sánchez <davidslt+git@pm.me>
2024-05-20 16:15:05 +0100

    test: replace yaml dependency

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
-{ mkDerivation, aeson, base, bytestring, hspec, hspec-discover, lib
-, QuickCheck, quickcheck-instances, text, unordered-containers
-, yaml
+{ mkDerivation, aeson, base, bytestring, hspec, hspec-discover
+, HsYAML, HsYAML-aeson, lib, QuickCheck, quickcheck-instances, text
+, unordered-containers
 }:
 mkDerivation {
   pname = "richenv";
-  version = "0.1.0.1";
+  version = "0.1.0.2";
   src = ./.;
   libraryHaskellDepends = [ aeson base text unordered-containers ];
   testHaskellDepends = [
-    aeson base bytestring hspec QuickCheck quickcheck-instances text
-    unordered-containers yaml
+    aeson base bytestring hspec HsYAML HsYAML-aeson QuickCheck
+    quickcheck-instances text unordered-containers
   ];
   testToolDepends = [ hspec-discover ];
   homepage = "https://github.com/DavSanchez/richenv";
